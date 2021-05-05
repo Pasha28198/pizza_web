8be72fdffc7e96d5d9e537d4816f25e6a5a1c78c
borrikon <borrikon@gmail.com>
2021-05-05 18:58:16 +0300

    create burger_hover menu

--- components/header/burgerIcon/index.js
+++ components/header/burgerIcon/index.js
@@ -1,4 +1,10 @@
+import Link from 'next/link'
+
 import { useMobileMenuCtx } from '../../../context/mibileMenuCtx'
+
+import precent from '../../../assets/headerIcons/percentBtn.svg'
+import megaphone from '../../../assets/headerIcons/megaphoneBtn.svg'
+import fast from '../../../assets/headerIcons/fastBtn.svg'
 import styles from './styles.module.scss'
 
 export default function BurgerIcon(){
@@ -15,11 +21,38 @@ export default function BurgerIcon(){
     }
 
     return(
-        <div
-            className={open ? styles.menuContX : styles.menuCont}
-            onClick={()=>menuHandler()}
-        >
-            <div />
+        <div>
+            <div
+                className={open ? styles.menuContX : styles.menuCont}
+                onClick={()=>menuHandler()}
+            >
+                <div />
+            </div>
+            <div className={styles.menuContBig}>
+                <div className={styles.burgerPart} />
+                <div className={styles.absCont}>
+                    <div className={styles.decktopMenu}>
+                        <Link href='#'>
+                            <button>
+                                <img src={precent} alt=''/>
+                                Акції
+                            </button>
+                        </Link>
+                        <Link href='#'>
+                            <button>
+                                <img src={megaphone} alt=''/>
+                                Новини
+                            </button>
+                        </Link>
+                        <Link href='#'>
+                            <button>
+                                <img src={fast} alt=''/>
+                                Доставка і оплата
+                            </button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
