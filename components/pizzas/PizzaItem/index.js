b200ef5f8d98f2397ddc13c7d10ad97c391fca0b
borrikon <borrikon@gmail.com>
2021-05-09 15:01:59 +0300

    connected constructor component

--- components/pizzas/PizzaItem/index.js
+++ components/pizzas/PizzaItem/index.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Link from 'next/link'
 
 import HeartSvg from '../../svgImg/heartSvg'
 import HeartYel from '../../svgImg/heartYelSvg'
@@ -13,41 +14,60 @@ export default function PizzaItem(){
 
     const [isLike, setIsLike] = useState(false)
 
+    const likeHandler = (e) => {
+        e.stopPropagation()
+        setIsLike(!isLike)
+    }
+
     return(
-        <div className={styles.itemContainer}>
-            <div className={styles.itemImg}>
-                <img src={pizzaImg} alt='pizza'/>
-                <img src={pizzaImgDeck} alt='pizza'/>
-            </div>
-            <div className={styles.itemContent}>
-                <div className={styles.itemMainInfo}>
-                    <div>
-                        <h4>Маргарита </h4>
-                        <p>250 гр</p>
+        <Link href='/pizzaConstructor/12' >
+            <div className={styles.itemContainer}>
+                <div className={styles.itemImg}>
+                    <img src={pizzaImg} alt='pizza'/>
+                    <img src={pizzaImgDeck} alt='pizza'/>
+                </div>
+                <div className={styles.itemContent}>
+                    <div className={styles.itemMainInfo}>
+                        <div>
+                            <h4>Маргарита </h4>
+                            <p>250 гр</p>
+                        </div>
+                        <div 
+                            onClick={(e)=>likeHandler(e)} 
+                            className={styles.svgCont}
+                        >
+                            {isLike ? <HeartYel /> : <HeartSvg />}
+                        </div>
                     </div>
-                    <div onClick={()=>setIsLike(!isLike)} className={styles.svgCont}>
-                        {isLike ? <HeartYel /> : <HeartSvg />}
+                    <div className={styles.itemSubtitle}>
+                        Соус вершковий, сир моцарела, сир рокфор, сир пармезан, сир брі...
+                    </div>
+                    <div className={styles.typeChoose}>
+                        <button
+                            onClick={(e)=>e.stopPropagation()}
+                            className={styles.btnType}
+                        >
+                            Італійська
+                        </button>
+                        <button 
+                            onClick={(e)=>e.stopPropagation()}
+                            className={styles.btnType}
+                        >
+                            Американська
+                        </button>
+                        <h4>220 грн</h4>
+                        <button 
+                            onClick={(e)=>e.stopPropagation()}
+                            className={styles.btnSubmit}
+                        >
+                            Замовити
+                        </button>
+                    </div>
+                    <div className={styles.addIng}>
+                        <div>+ Додати інгрідієнти</div>
                     </div>
                 </div>
-                <div className={styles.itemSubtitle}>
-                    Соус вершковий, сир моцарела, сир рокфор, сир пармезан, сир брі...
-                </div>
-                <div className={styles.typeChoose}>
-                    <button className={styles.btnType}>
-                        Італійська
-                    </button>
-                    <button className={styles.btnType}>
-                        Американська
-                    </button>
-                    <h4>220 грн</h4>
-                    <button className={styles.btnSubmit}>
-                        Замовити
-                    </button>
-                </div>
-                <div className={styles.addIng}>
-                    <div>+ Додати інгрідієнти</div>
-                </div>
             </div>
-        </div>
+        </Link>
     )
 }
