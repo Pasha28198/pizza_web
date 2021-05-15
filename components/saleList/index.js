b5a811c8d2c0242dd04d2c3a6c9ab60141bf3dd5
borrikon <borrikon@gmail.com>
2021-05-15 15:33:53 +0300

    created pagination for sale_page

--- components/saleList/index.js
+++ components/saleList/index.js
@@ -1,15 +1,40 @@
+import { useState } from 'react'
+import Pagination from './pagination'
 import SaleItem from './saleItem'
 import styles from './styles.module.scss'
 
 export default function SaleList(){
+
+    const postArr = []
+    for(let i = 0; i <= 48; i++){postArr.push(<SaleItem key={i}/>)}
+
+    const [posts, setPosts] = useState(postArr)
+    const [currentPage, setCurrentPage] = useState(1)
+    const [postPerPage] = useState(8)
+
+    const indexOfLastPost = currentPage * postPerPage
+    const indexOfFirstPost = indexOfLastPost - postPerPage
+    const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost)
+
+    const paginate = pageNumber => {
+        if(pageNumber <= 0 || pageNumber>Math.ceil(posts.length/postPerPage)){
+            return
+        }
+        setCurrentPage(pageNumber)
+    }
+
     return(
         <div className={styles.contTitle}>
             <h1>Акції</h1>
             <div className={styles.listCont}>
-                <SaleItem />
-                <SaleItem />
-                <SaleItem />
+                {currentPosts.map((i)=>i)}
             </div>
+            <Pagination
+                currentPage={currentPage} 
+                postPerPage={postPerPage}
+                totalPosts={posts.length}
+                paginate={paginate}
+            />
         </div>
     )
 } 
