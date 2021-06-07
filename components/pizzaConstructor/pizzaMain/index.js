9de21a05fc5438e162b6d06065ed66a912c547df
Pasha <pavlognidin@gmail.com>
2021-06-07 13:28:36 +0300

    add img

--- components/pizzaConstructor/pizzaMain/index.js
+++ components/pizzaConstructor/pizzaMain/index.js
@@ -1,110 +1,113 @@
-import {useState, useEffect, useContext} from 'react'
+import { useState, useEffect, useContext } from "react";
 
-import Backdrop from '../../backdrop'
-import ModalConst from '../modalConstr'
+import Backdrop from "../../backdrop";
+import ModalConst from "../modalConstr";
 
-import HeartSvg from '../../svgImg/heartSvg'
-import HeartYelSvg from '../../svgImg/heartYelSvg'
+import HeartSvg from "../../svgImg/heartSvg";
+import HeartYelSvg from "../../svgImg/heartYelSvg";
 
-import styles from './styles.module.scss'
+import styles from "./styles.module.scss";
 
-import pizzaMob from '../../../assets/pizzaConstImg/pizzaMob.png'
-import pizzaBig from '../../../assets/pizzaConstImg/pizzaBig.png'
-import fast from '../../../assets/pizzaConstImg/fast.svg'
-import {useRouter} from 'next/router'
-import {ProductsStoreInstanceCTX} from "../../../stores/PostStore";
-import {observer} from "mobx-react";
-import {BasketStoreInstanceCTX} from "../../../stores/basket_store";
-
+import pizzaMob from "../../../assets/pizzaConstImg/pizzaMob.png";
+import pizzaBig from "../../../assets/pizzaConstImg/pizzaBig.png";
+import fast from "../../../assets/pizzaConstImg/fast.svg";
+import { useRouter } from "next/router";
+import { ProductsStoreInstanceCTX } from "../../../stores/PostStore";
+import { observer } from "mobx-react";
+import { BasketStoreInstanceCTX } from "../../../stores/basket_store";
 
 const PizzaMain = observer(() => {
-    const {getProductsId, product} = useContext(ProductsStoreInstanceCTX)
+  const { getProductsId, product } = useContext(ProductsStoreInstanceCTX);
 
-    const router = useRouter()
+  const router = useRouter();
 
-    useEffect(() => {
-        if (router?.query?.name) {
-            getProductsId(router?.query?.name).then(res => {
-                setChoiseActive(res.data.choise[0])
-            })
-        }
-
-    }, [router.query.name])
-
-    const [isLike, setIsLike] = useState(false)
-    const [isOpen, setIsOpen] = useState(false)
-
-    const modalHandler = (e) => {
-        e.preventDefault()
-        setIsOpen(!isOpen)
+  useEffect(() => {
+    if (router?.query?.name) {
+      getProductsId(router?.query?.name).then((res) => {
+        setChoiseActive(res.data.choise[0]);
+      });
     }
+  }, [router.query.name]);
 
-    const [choiseActive, setChoiseActive] = useState({type: 'ghhj'})
+  const [isLike, setIsLike] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    return (
-        <div className='container'>
-            <div className={styles.mainCont}>
-                {
-                    isOpen
-                        ? <Backdrop>
-                            <ModalConst product={product} choiseActive={choiseActive} modalHandler={modalHandler}/>
-                        </Backdrop>
-                        : null
-                }
-                <div className={styles.imgCont}>
-                    <img src={pizzaMob} alt='pizza'/>
-                    <img src={pizzaBig} alt='pizza'/>
-                </div>
-                <div className={styles.infoMainCont}>
-                    <div className={styles.title}>
-                        <h2>{product.title}</h2>
-                        <p>{choiseActive.mass} гр</p>
-                    </div>
-                    <div className={styles.subtitle}>
-                        <p>
-                            {product?.ingredients?.map((item) => {
-                                return item.ingredient.name
-                            }).toString().split(',').join(', ')}
-                        </p>
-                    </div>
-                    <div className={styles.typeBtn}>
-                        {product?.choise?.map((item, index) => {
+  const modalHandler = (e) => {
+    e.preventDefault();
+    setIsOpen(!isOpen);
+  };
 
-                            return <button
-                                className={`${styles.btn} ${choiseActive.type === item.type ? styles.activeChoise : ''}`}
-                                onClick={() => {
-                                    setChoiseActive(item)
-                                }}>{item.type}</button>
-                        })}
-                    </div>
-                    <div className={styles.infoPizza}>
-                        <h4>{choiseActive.price} грн</h4>
-                        <button>Замовити</button>
-                        <div onClick={() => setIsLike(!isLike)}>
-                            {
-                                isLike ?
-                                    <HeartYelSvg/>
-                                    : <HeartSvg/>
-                            }
-                        </div>
-                    </div>
-                    <div
-                        onClick={(e) => modalHandler(e)}
-                        className={styles.ingrid}
-                    >
-                        Змінити інгрідієнти
-                    </div>
-                    <div className={styles.deliv}>
-                        <img src={fast} alt='delivery'/>
-                        <p>
-                            Безкоштовна доставка при
-                            замовленні від 200 грн
-                        </p>
-                    </div>
-                </div>
-            </div>
+  const [choiseActive, setChoiseActive] = useState({ type: "ghhj" });
+
+  return (
+    <div className="container">
+      <div className={styles.mainCont}>
+        {isOpen ? (
+          <Backdrop>
+            <ModalConst
+              product={product}
+              choiseActive={choiseActive}
+              modalHandler={modalHandler}
+            />
+          </Backdrop>
+        ) : null}
+        <div className={styles.imgCont}>
+          <img src={pizzaMob} alt="pizza" />
+          <img
+            src={`${process.env.NEXT_PUBLIC_URL}${product.img}`}
+            alt="pizza"
+          />
         </div>
-    )
-})
+        <div className={styles.infoMainCont}>
+          <div className={styles.title}>
+            <h2>{product.title}</h2>
+            <p>{choiseActive.mass} гр</p>
+          </div>
+          <div className={styles.subtitle}>
+            <p>
+              {product?.ingredients
+                ?.map((item) => {
+                  return item.ingredient.name;
+                })
+                .toString()
+                .split(",")
+                .join(", ")}
+            </p>
+          </div>
+          <div className={styles.typeBtn}>
+            {product?.choise?.map((item, index) => {
+              return (
+                <button
+                  className={`${styles.btn} ${
+                    choiseActive.type === item.type ? styles.activeChoise : ""
+                  }`}
+                  onClick={() => {
+                    setChoiseActive(item);
+                  }}
+                >
+                  {item.type}
+                </button>
+              );
+            })}
+          </div>
+          <div className={styles.infoPizza}>
+            <h4>{choiseActive.price} грн</h4>
+            <button>Замовити</button>
+            <div onClick={() => setIsLike(!isLike)}>
+              {isLike ? <HeartYelSvg /> : <HeartSvg />}
+            </div>
+          </div>
+          <div onClick={(e) => modalHandler(e)} className={styles.ingrid}>
+            Змінити інгрідієнти
+          </div>
+          <div className={styles.deliv}>
+            <img src={fast} alt="delivery" />
+            <p>Безкоштовна доставка при замовленні від 200 грн</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+});
 
-export default PizzaMain
+export default PizzaMain;
--- components/pizzas/PizzaItem/index.js
+++ components/pizzas/PizzaItem/index.js
@@ -1,102 +1,101 @@
-import {useState, useEffect, useContext} from 'react'
-import Link from 'next/link'
+import { useState, useEffect, useContext } from "react";
+import Link from "next/link";
 
-import HeartSvg from '../../svgImg/heartSvg'
-import HeartYel from '../../svgImg/heartYelSvg'
+import HeartSvg from "../../svgImg/heartSvg";
+import HeartYel from "../../svgImg/heartYelSvg";
 
-import styles from './styles.module.scss'
+import styles from "./styles.module.scss";
 
-import pizzaImg from '../../../assets/pizzaItemImg/pizza.png'
-import pizzaImgDeck from '../../../assets/pizzaItemImg/pizzaDeck.png'
-import {BasketStoreInstanceCTX} from "../../../stores/basket_store";
-import {ProductsStoreInstanceCTX} from "../../../stores/PostStore";
+import pizzaImg from "../../../assets/pizzaItemImg/pizza.png";
+import pizzaImgDeck from "../../../assets/pizzaItemImg/pizzaDeck.png";
+import { BasketStoreInstanceCTX } from "../../../stores/basket_store";
+import { ProductsStoreInstanceCTX } from "../../../stores/PostStore";
 
+const PizzaItem = ({ item }) => {
+  const [isLike, setIsLike] = useState(false);
+  const { productToBasket } = useContext(ProductsStoreInstanceCTX);
+  const { addProduct } = useContext(BasketStoreInstanceCTX);
 
-const PizzaItem = ({item}) => {
+  const likeHandler = (e) => {
+    e.stopPropagation();
+    setIsLike(!isLike);
+  };
 
-    const [isLike, setIsLike] = useState(false)
-    const { productToBasket } = useContext(ProductsStoreInstanceCTX);
-    const {addProduct} = useContext(BasketStoreInstanceCTX)
+  useEffect(() => {
+    setChoiseActive(item.choise[0]);
+  }, []);
 
+  const [choiseActive, setChoiseActive] = useState({});
 
-    const likeHandler = (e) => {
-        e.stopPropagation()
-        setIsLike(!isLike)
-    }
-
-    useEffect(() => {
-        setChoiseActive(item.choise[0])
-
-    }, [])
-
-    const [choiseActive, setChoiseActive] = useState({})
-
-    const addProductToBasket = () => {
-        const product = productToBasket(item._id, choiseActive._id);
-        addProduct(product);
-    }
-
-    return (
-
-        <div className={styles.itemContainer}>
-            <Link href={`/pizzaConstructor/${item._id}`}>
-                <div className={styles.itemImg}>
-                    <img src={pizzaImg} alt='pizza'/>
-                    <img src={pizzaImgDeck} alt='pizza'/>
-                </div>
-            </Link>
-            <div className={styles.itemContent}>
-                <Link href={`/pizzaConstructor/${item._id}`}>
-                    <div className={styles.itemMainInfo}>
-                        <div>
-                            <h4>{item.title} </h4>
-                            <p>{choiseActive?.mass} гр</p>
-                        </div>
-                        <div
-                            onClick={(e) => likeHandler(e)}
-                            className={styles.svgCont}
-                        >
-                            {isLike ? <HeartYel/> : <HeartSvg/>}
-                        </div>
-                    </div>
-                </Link>
-                <div className={styles.itemSubtitle}>
-
-                    {item?.ingredients?.map((item) => {
-                        return item.ingredient.name
-                    }).toString().split(',').join(', ')}
-                </div>
-                <div className={styles.typeChoose}>
-                    {item?.choise?.map((item) => {
-                        return <button
-                            className={`${styles.btnType} ${choiseActive?._id === item._id ? styles.activeChoise : ''}`}
-                            onClick={() => {
-                                setChoiseActive(item)
-                            }}>{item.type}</button>
-                    })}
-
-                </div>
-                <div style={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                    padding: '0 24px'
-                }}>
-                    <h4>{choiseActive?.price} грн</h4>
-                    <button
-                        onClick={addProductToBasket}
-                        className={styles.btnSubmit}
-                    >
-                        Замовити
-                    </button>
-                </div>
-                <div className={styles.addIng}>
-                    <div>+ Додати інгрідієнти</div>
-                </div>
-            </div>
+  const addProductToBasket = () => {
+    const product = productToBasket(item._id, choiseActive._id);
+    addProduct(product);
+  };
+  console.log(process.env.NEXT_PUBLIC_URL);
+  return (
+    <div className={styles.itemContainer}>
+      <Link href={`/pizzaConstructor/${item._id}`}>
+        <div className={styles.itemImg}>
+          <img src={pizzaImg} alt="pizza" />
+          <img src={`${process.env.NEXT_PUBLIC_URL}${item.img}`} alt="pizza" />
         </div>
-
-    )
-}
+      </Link>
+      <div className={styles.itemContent}>
+        <Link href={`/pizzaConstructor/${item._id}`}>
+          <div className={styles.itemMainInfo}>
+            <div>
+              <h4>{item.title} </h4>
+              <p>{choiseActive?.mass} гр</p>
+            </div>
+            <div onClick={(e) => likeHandler(e)} className={styles.svgCont}>
+              {isLike ? <HeartYel /> : <HeartSvg />}
+            </div>
+          </div>
+        </Link>
+        <div className={styles.itemSubtitle}>
+          {item?.ingredients
+            ?.map((item) => {
+              return item.ingredient.name;
+            })
+            .toString()
+            .split(",")
+            .join(", ")}
+        </div>
+        <div className={styles.typeChoose}>
+          {item?.choise?.map((item) => {
+            return (
+              <button
+                className={`${styles.btnType} ${
+                  choiseActive?._id === item._id ? styles.activeChoise : ""
+                }`}
+                onClick={() => {
+                  setChoiseActive(item);
+                }}
+              >
+                {item.type}
+              </button>
+            );
+          })}
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            padding: "0 24px",
+          }}
+        >
+          <h4>{choiseActive?.price} грн</h4>
+          <button onClick={addProductToBasket} className={styles.btnSubmit}>
+            Замовити
+          </button>
+        </div>
+        <div className={styles.addIng}>
+          <div>+ Додати інгрідієнти</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default PizzaItem;
